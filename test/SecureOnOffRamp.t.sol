6562f23cd2054e4555762a84f34e1c148a9db855
Enockkipkoech <enockkipkoech19@gmail.com>
2024-05-06 20:14:31 +0300

    :rocket: Beginning of the Next Big THing! :fire:

---
+++ test/SecureOnOffRamp.t.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {SecureOnOffRamp} from "../src/SecureOnOffRamp.sol";
+
+contract SecureOnOffRampTest is Test {
+    SecureOnOffRampTest  public secureOnOffRamp;
+
+    function setUp() public {
+    }   
+}
