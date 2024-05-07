f19e800d0ffa5faca0af6a8460f77627bb83c945
Enockkipkoech <enockkipkoech19@gmail.com>
2024-05-07 21:49:25 +0300

    :rocket: Installed ccip interfaces contracts :white_check_mark:

--- src/SecureOnOffRamp.sol
+++ src/SecureOnOffRamp.sol
@@ -1,7 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.25;
 
-import { IRouterClient} from "@chainlink/contracts/src/v0.8/interfaces/IRouterClient.sol";
+import { IRouterClient} from "@chainlink/contracts-ccip/src/v0.8/ccip/interfaces/IRouterClient.sol";
+import { OwnerIsCreator } from "@chainlink/contracts-ccip/src/v0.8/shared/access/OwnerIsCreator.sol";
+import { Client } from "@chainlink/contracts-ccip/src/v0.8/ccip/libraries/Client.sol";
+import { LinkTokenInterface } from "@chainlink/contracts/src/v0.8/shared/interfaces/LinkTokenInterface.sol";
+import { CCIPReceiver } from "@chainlink/contracts-ccip/src/v0.8/ccip/applications/CCIPReceiver.sol";
+
+
+
+
 
 
 
